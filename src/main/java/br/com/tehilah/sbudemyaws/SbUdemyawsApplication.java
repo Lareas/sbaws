4eed9a5017816c245395840ff6286e47fbf8a8c8
Luiz Fernando Arêas <lfernandoareas@gmail.com>
2021-08-05 06:15:16 -0300

    sbaws 1o commit

---
+++ src/main/java/br/com/tehilah/sbudemyaws/SbUdemyawsApplication.java
@@ -0,0 +1,13 @@
+package br.com.tehilah.sbudemyaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbUdemyawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbUdemyawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tehilah/sbudemyaws/SbUdemyawsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tehilah.sbudemyaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbUdemyawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
